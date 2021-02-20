942410aed00248855ecf3bc6847b08b56b7966cf
Andrew <35616217+Shankapotamus66@users.noreply.github.com>
2021-02-19 17:03:50 -0800

    Revert "Frontend testing"

---
+++ README.md
@@ -0,0 +1,2 @@
+# CMPS4910-Senior-Project
+Git Repo for PATH-O-GEN, CMPS 4910 and CMPS 4920 Senior Project.
--- contact-tracing-app/api/constants.js
+++
@@ -1,6 +0,0 @@
-
-//Store constant values for reuse
-const api = "https://path-o-gen-rest-api.herokuapp.com";
-export {
-    api
-};
