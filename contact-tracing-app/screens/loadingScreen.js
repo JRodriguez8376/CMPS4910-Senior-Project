0ea0d1e148795560ce685299729e7e27a8ceed1c
Joshua Rodriguez Home <jrod057197@gmail.com>
2021-03-01 12:34:59 -0800

    Committing current working frontend to main test branch from frontend_testing

---
+++ contact-tracing-app/screens/loadingScreen.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import {
+    Text,
+    View,
+    StyleSheet,
+    TextInput,
+    ImageBackground,
+    TouchableOpacity,
+    Image,
+    ActivityIndicator
+} from 'react-native';
+
+const LoadingScreen = () => {
+    return (
+        <View>
+            <Text> This is a loading screen</Text>
+        </View>
+    );
+}
+
+export default LoadingScreen;
