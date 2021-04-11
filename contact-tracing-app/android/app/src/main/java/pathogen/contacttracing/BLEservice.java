bf025ca86f81b3b1dcf6d1ee3c5f53e9973fbad4
avalencia17 <47010418+avalencia17@users.noreply.github.com>
2021-04-11 13:11:33 -0700

    a

--- contact-tracing-app/android/app/src/main/java/pathogen/contacttracing/BLEservice.java
+++ contact-tracing-app/android/app/src/main/java/pathogen/contacttracing/BLEservice.java
@@ -1,4 +1,19 @@
 package pathogen.contacttracing;
 
-public class BLEservice {
+import android.bluetooth.BluetoothAdapter;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+
+public class BLEservice extends ReactContextBaseJavaModule {
+    public static BLE ble;
+    BluetoothAdapter bluetoothAdapter;
+
+    @NonNull
+    @Override
+    public String getName() {
+        return null;
+    }
 }
+
